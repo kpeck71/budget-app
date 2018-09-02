f7ad627264d43a4f89f60af624034a20400eb6d5
kpeck71 <kpeck71@gmail.com>
2018-09-02 09:26:58 -0400

    budget form receiving props

--- client/src/components/budget/BudgetInput.js
+++ client/src/components/budget/BudgetInput.js
@@ -3,18 +3,19 @@ import React, { Component } from 'react';
 const BudgetInput = props => {
   return (
 
-  <div className="BudgetInput">
-    <h1>Here is where my Budget form will go</h1>
-
-    <form>
-      <input type="number" name="income" value={this.props.income} placeholder="Monthly Income" />
-      <input type="number" name="expenses" value={this.props.expenses} placeholder="Expense" />
-      <input type="number" name="expenses" value={this.props.expenses} placeholder="Expense" />
-      <input type="number" name="expenses" value={this.props.expenses} placeholder="Expense" />
-      <input type="submit" />
-    </form>
-
-  </div>
+    <div className="BudgetInput">
+      <form onSubmit={props.handleSubmit}>
+        <input type="text" name="income" value={props.income } placeholder="Monthly Income" onChange={props.handleChange} />
+        <input type="text" name="housing_cost" value={props.housing_cost } placeholder="Housing Costs" onChange={props.handleChange} />
+        <input type="text" name="food" value={props.food } placeholder="Food" onChange={props.handleChange} />
+        <input type="text" name="credit_cards" value={props.credit_cards } placeholder="Credit Cards" onChange={props.handleChange} />
+        <input type="text" name="car_loan" value={props.car_loan } placeholder="Car Loans" onChange={props.handleChange} />
+        <input type="text" name="personal_loan" value={props.personal_loan } placeholder="Personal Loans" onChange={props.handleChange} />
+        <input type="text" name="savings" value={props.savings } placeholder="Savings" onChange={props.handleChange} />
+        <input type="text" name="miscellaneous" value={props.miscellaneous } placeholder="Miscellaneous" onChange={props.handleChange} />
+        <input type="submit" />
+      </form>
+    </div>
 
   )
 
--- client/src/containers/BudgetContainer.js
+++ client/src/containers/BudgetContainer.js
@@ -6,14 +6,18 @@ import BudgetInput from '../components/budget/BudgetInput'
 class BudgetContainer extends React.Component  {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      id: '',
       income: '',
-      expenses: ''
+      housing_cost: '',
+      food: '',
+      credit_cards: '',
+      car_loan: '',
+      personal_loan: '',
+      savings: '',
+      miscellaneous: ''
     }
   }
-  //
   // componentDidMount()  {
   //    fetch('/api/v1/budgets.json')
   //     .then(response => response.json())
@@ -23,13 +27,20 @@ class BudgetContainer extends React.Component  {
   //       })
   //     });
   //   }
+  handleChange = event => {
+    console.log("change handled")
+  }
 
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log("event submitted")
+  }
 
   render() {
     return (
       <div className="budgetContainer">
       {/*  {this.state.budget.map((budget) => budget.income)} */}
-      <BudgetInput income={this.state.income} expenses={this.state.expenses} />
+      <BudgetInput handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
       </div>
     )
   }
